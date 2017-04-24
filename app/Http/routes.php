2efcfd5778357b3b45dfdb0bf9acde9d484b8336
Jacob Monnikendam <jacobmonn@sbcglobal.net>
2017-04-24 15:55:20 -0500

    adds "/rolldice/" route

--- app/Http/routes.php
+++ app/Http/routes.php
@@ -17,23 +17,32 @@ if (isset($_SERVER['REQUEST_URI']) && substr($_SERVER['REQUEST_URI'], -1) !== '/
     die;
 }
 
-Route::get('/', function () {
+Route::get('/', function ()
+{
     return view('welcome');
 });
 
-Route::get('/helloworld/', function () {
+Route::get('/helloworld/', function ()
+{
     return 'hello world';
 });
 
-Route::get('/uppercase/{word?}/', function ($word = 'uppercase') {
+Route::get('/uppercase/{word?}/', function ($word = 'uppercase')
+{
     return strtoupper($word);
 });
 
-Route::get('/lowercase/{word?}/', function($word = 'LOWERCASE') {
+Route::get('/lowercase/{word?}/', function ($word = 'LOWERCASE')
+{
     return strtolower($word);
 });
 
-Route::get('/add/{a?}/{b?}', function($a = 0, $b = 0) {
-    $o = $b > 0 ? '+' : '-';
+Route::get('/add/{a?}/{b?}/', function ($a = 0, $b = 0)
+{
+    $o = $b < 0 ? '-' : '+';
     return "$a $o " . abs($b) . ' = ' . ($a + $b);
+});
+
+Route::get('/rolldice/', function () {
+    return mt_rand(1, 6);
 });
