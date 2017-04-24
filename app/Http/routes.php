ebd1ead5881b866d13279fe1949b64fe1a985890
Jacob Monnikendam <jacobmonn@sbcglobal.net>
2017-04-24 16:09:42 -0500

    adds view for rolldice

--- app/Http/routes.php
+++ app/Http/routes.php
@@ -13,36 +13,37 @@
 
 // first things first, let's make sure that url is all pretty
 if (isset($_SERVER['REQUEST_URI']) && substr($_SERVER['REQUEST_URI'], -1) !== '/') {
-    header("Location: {$_SERVER['REQUEST_URI']}/");
-    die;
+	header("Location: {$_SERVER['REQUEST_URI']}/");
+	die;
 }
 
 Route::get('/', function ()
 {
-    return view('welcome');
+	return view('welcome');
 });
 
 Route::get('/helloworld/', function ()
 {
-    return 'hello world';
+	return 'hello world';
 });
 
 Route::get('/uppercase/{word?}/', function ($word = 'uppercase')
 {
-    return strtoupper($word);
+	return strtoupper($word);
 });
 
 Route::get('/lowercase/{word?}/', function ($word = 'LOWERCASE')
 {
-    return strtolower($word);
+	return strtolower($word);
 });
 
 Route::get('/add/{a?}/{b?}/', function ($a = 0, $b = 0)
 {
-    $o = $b < 0 ? '-' : '+';
-    return "$a $o " . abs($b) . ' = ' . ($a + $b);
+	$o = $b < 0 ? '-' : '+';
+	return "$a $o " . abs($b) . ' = ' . ($a + $b);
 });
 
 Route::get('/rolldice/', function () {
-    return mt_rand(1, 6);
+	$roll = mt_rand(1, 6);
+	return view('roll_dice', ['roll' => $roll]);
 });
