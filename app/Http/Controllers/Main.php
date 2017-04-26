8747abc435e29c293948c9befe0da31dcf3db0e9
Jacob Monnikendam <jacobmonn@sbcglobal.net>
2017-04-26 14:00:55 -0500

    adds page title to every page; removes unnecessary file

--- app/Http/Controllers/Main.php
+++ app/Http/Controllers/Main.php
@@ -15,6 +15,8 @@ class Main extends Controller {
 
     public function add($x = 0, $y = 0)
     {
+        $data['pageTitle'] = 'Add';
+        
         $data['x'] = $x;
         $data['o'] = $y < 0 ? '-' : '+';
         $data['y'] = $y;
@@ -22,9 +24,11 @@ class Main extends Controller {
         return view('add', $data);
     }
 
-    public function rolldice($guess = NULL) {
-        $data['roll'] = mt_rand(1, 6);
+    public function rolldice($guess = NULL)
+    {
+        $data['pageTitle'] = 'Roll';
 
+        $data['roll'] = mt_rand(1, 6);
         $data['guess'] = $guess;
 
         return view('roll_dice', $data);
@@ -32,8 +36,30 @@ class Main extends Controller {
 
     public function increment($number = 0)
     {
+        $data['pageTitle'] = "$number | Increment";
+
         $data['number'] = $number;
 
         return view('increment', $data);
+    }
+
+    public function uppercase($word = 'uppercase')
+    {
+        $data['pageTitle'] = 'UPPERCASE';
+
+        $data['word'] = $word;
+        $data['newWord'] = strtoupper($word);
+
+        return view('convert_case', $data);
+    }
+
+    public function lowercase($word = 'LOWERCASE')
+    {
+        $data['pageTitle'] = 'lowercase';
+        
+        $data['word'] = $word;
+        $data['newWord'] = strtolower($word);
+
+        return view('convert_case', $data);
     }
 }
