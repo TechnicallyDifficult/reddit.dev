442b211056a3d852cc7df23771e7324ab17c8d17
Jacob Monnikendam <jacobmonn@sbcglobal.net>
2017-04-26 14:05:47 -0500

    rolldice page now uses laravel blade

---
+++ resources/views/roll_dice.blade.php
@@ -0,0 +1,15 @@
+@extends('layouts.master')
+
+@section('content')
+    <p>{{ $roll }}</p>
+    @if (isset($guess))
+        <p>{{ $guess }}</p>
+        <p>
+            @if ($guess == $roll)
+                Looks like a match.
+            @else
+                Nope. Wrong. Luck is depressing, isn't it?
+            @endif
+        </p>
+    @endif
+@stop
