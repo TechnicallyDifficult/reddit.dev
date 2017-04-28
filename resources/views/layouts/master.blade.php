014fc9761cdfd23a1af03c637617e6752c3f957d
Jacob Monnikendam <jacobmonn@sbcglobal.net>
2017-04-28 15:12:57 -0500

    adds some comments

--- resources/views/layouts/master.blade.php
+++ resources/views/layouts/master.blade.php
@@ -1,9 +1,9 @@
 <!DOCTYPE html>
 <html>
 <head>
+    <!-- Bootstrap -->
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1">
-
     <link
         rel="stylesheet"
         href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css"
@@ -21,17 +21,27 @@
     </title>
 </head>
 <body>
-    <main class="container">
+    <main class="container-fluid">
         @yield('content')
     </main>
+
+    @hasSection('sidebar')
+        <aside>
+            @yield('sidebar')
+        </aside>
+    @endif
+
+    <!-- jQuery -->
     <script
         src="https://code.jquery.com/jquery-3.2.1.min.js"
         integrity="sha256-hwg4gsxgFZhOsEEamdOYGBf13FyQuiTwlAQgxVSNgt4="
         crossorigin="anonymous"></script>
+    <!-- Bootstrap JS -->
     <script
         src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js"
         integrity="sha384-Tc5IQib027qvyjSMfHjOMaLkfuWVxZxUPnCJA7l2mCWNIpG9mGCD8wGNIcPD7Txa"
         crossorigin="anonymous"></script>
+
     @yield('script')
 </body>
 </html>
