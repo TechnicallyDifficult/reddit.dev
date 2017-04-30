f35d26eb2e94c7bd00edac5af659437f25fec4ed
Jacob Monnikendam <jacobmonn@sbcglobal.net>
2017-04-30 18:13:06 -0500

    adds JS for swapping time displays

--- public/js/time_click.js
+++ public/js/time_click.js
@@ -1,15 +1,10 @@
 $(document).ready(function() {
 	function swapTime()
 	{
-		var $this = $(this);
-
-		if ($this.text() === $this.attr('data-datetime-relative')) {
-			$this.text($this.attr('data-datetime'));
-		} else if ($this.text() === $this.attr('data-datetime')) {
-			$this.text($this.attr('data-datetime-relative'));
-		} else {
-			$this.text('You screwed something up, didn\'t you?');
-		}
+		$(this)
+			.children('time')
+			.children('span')
+			.toggleClass('hidden');
 	}
 
 	$('a, .time').each(function(index, el) {
