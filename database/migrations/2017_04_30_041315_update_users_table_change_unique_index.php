482d48e24a53ee261381f6bfc4895aa3f1924808
Jacob Monnikendam <jacobmonn@sbcglobal.net>
2017-04-30 00:03:47 -0500

    adds new column "username" to "users" table and makes it unique instead of email

---
+++ database/migrations/2017_04_30_041315_update_users_table_change_unique_index.php
@@ -0,0 +1,33 @@
+<?php
+
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class UpdateUsersTableChangeUniqueIndex extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::table('users', function($table) {
+            $table->dropUnique(['email']);
+            $table->string('username')->unique();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::table('users', function($table) {
+            $table->dropColumn('username');
+            $table->unique('email');
+        });
+    }
+}
