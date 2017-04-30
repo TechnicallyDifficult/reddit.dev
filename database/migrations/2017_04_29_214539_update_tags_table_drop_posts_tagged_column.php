b4958f1de0df9016244cc36b11502ea05d60f3e8
Jacob Monnikendam <jacobmonn@sbcglobal.net>
2017-04-29 23:10:42 -0500

    changes filenames again (changed my mind)

--- database/migrations/2017_04_29_214539_update_tags_table_drop_posts_tagged_column.php
+++ database/migrations/2017_04_29_214539_update_tags_table_drop_posts_tagged_column.php
@@ -3,7 +3,7 @@
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Database\Migrations\Migration;
 
-class UpdateTagsTableDropPoststaggedColumn extends Migration
+class UpdateTagsTableDropPostsTaggedColumn extends Migration
 {
     /**
      * Run the migrations.
