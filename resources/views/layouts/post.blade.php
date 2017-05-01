4fe1052e13fa1e7bf3c6d62d33f14a57fd7f340f
Jacob Monnikendam <jacobmonn@sbcglobal.net>
2017-05-01 11:38:15 -0500

    updates to work with new formats

--- resources/views/layouts/post.blade.php
+++ resources/views/layouts/post.blade.php
@@ -5,21 +5,21 @@
 @stop
 
 @section('content')
-    <article id="post">
-        <div class="row">
-            <div class="col-sm-8 col-md-9">
-                <header class="post-title">
-                    <h1>@yield('postTitle')</h1>
-                </header>
-                @yield('post')
-            </div>
+    <article id="post" class="col-sm-8 col-md-9">
+        <header>
+            <h1>@yield('postTitle')</h1>
+        </header>
+        <div class="section-body">
+            @yield('post')
         </div>
         @hasSection('comments')
-            <aside id="comments" class="row">
-                <header class="comments-header">
-                    <h1>Comments</h1>
+            <aside id="comments">
+                <header>
+                    <h1>{{-- {{ NUMBER_OF_COMMENTS }} --}} Comments</h1>
                 </header>
-                @yield('comments')
+                <div class="section-body">
+                    @yield('comments')
+                </div>
             </aside>
         @endif
     </article>
