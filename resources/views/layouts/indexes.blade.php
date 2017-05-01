b13bd292434eda0428512813d8d4e98b6f1e5642
Jacob Monnikendam <jacobmonn@sbcglobal.net>
2017-05-01 11:55:29 -0500

    adds layout for indexes

---
+++ resources/views/layouts/indexes.blade.php
@@ -0,0 +1,12 @@
+@extends('master')
+
+@section('content')
+    <article id="index" class="col-sm-8 col-md-9">
+        <header class="index-header">
+            <h1>@yield('index-header')</h1>
+        </header>
+        <div>
+            
+        </div>
+    </article>
+@stop
