1869186214d845cedb8f70986ce898d3d20554e3
Jacob Monnikendam <jacobmonn@sbcglobal.net>
2017-05-02 12:49:12 -0500

    removes indexes.blade.php; puts everything it had and more into posts/index.blade.php; abstracts some things into index_entry.blade.php

--- resources/views/posts/index.blade.php
+++ resources/views/posts/index.blade.php
@@ -1,5 +1,28 @@
 @extends('master')
 
+@section('style')
+	<link rel="stylesheet" href="/css/posts.css">
+@stop
+
+@section('pageTitle', 'All Posts')
+
 @section('content')
-    
+	<article id="posts-index" class="index">
+		<header>
+			<h1>All Posts</h1>
+		</header>
+		<submain>
+			<ol>
+				@foreach ($stickies as $post)
+					@include('posts.index_entry')
+				@endforeach
+			</ol>
+			<ol>
+				@foreach ($posts as $post)
+					@include('posts.index_entry')
+				@endforeach
+			</ol>
+		</submain>
+		@include('partials.pagelinks_nav')
+	</article>
 @stop
