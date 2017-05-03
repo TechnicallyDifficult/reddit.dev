5ac846568a0148dfc13e33a4986feedef6f2bb03
Jacob Monnikendam <jacobmonn@sbcglobal.net>
2017-05-02 22:50:18 -0500

    pages now display error messages properly and check for empty values before submitting

--- app/Http/Controllers/PostsController.php
+++ app/Http/Controllers/PostsController.php
@@ -6,131 +6,150 @@ use Illuminate\Http\Request;
 
 use App\Http\Requests;
 use App\Http\Controllers\Controller;
+use \App\Models\Post;
 
 class PostsController extends Controller
 {
-    protected function getLocalVars($vars)
-    {
-        $ignore = ['GLOBALS', '_SERVER', '_GET', '_POST', '_FILES', '_REQUEST', '_SESSION', '_ENV', '_COOKIE', 'php_errormsg', 'HTTP_RAW_POST_DATA', 'http_response_header', 'argc', 'argv'];
+	protected function getLocalVars($vars)
+	{
+		$ignore = ['GLOBALS', '_SERVER', '_GET', '_POST', '_FILES', '_REQUEST', '_SESSION', '_ENV', '_COOKIE', 'php_errormsg', 'HTTP_RAW_POST_DATA', 'http_response_header', 'argc', 'argv'];
 
-        foreach ($ignore as $name) {
-            if (isset($vars[$name])) {
-                unset($vars[$name]);
-            }
-        }
+		foreach ($ignore as $name) {
+			if (isset($vars[$name])) {
+				unset($vars[$name]);
+			}
+		}
 
-        return $vars;
-    }
+		return $vars;
+	}
 
-    /**
-     * Display a listing of the resource.
-     *
-     * @return \Illuminate\Http\Response
-     */
-    public function index()
-    {
-        // list all posts
+	/**
+	 * Display a listing of the resource.
+	 *
+	 * @return \Illuminate\Http\Response
+	 */
+	public function index()
+	{
+		// list all posts
 
-        $posts = \App\Models\Post::all();
+		$posts = Post::all();
 
-        return view('posts.index', $this->getLocalVars(get_defined_vars()));
-    }
+		return view('posts.index', $this->getLocalVars(get_defined_vars()));
+	}
 
-    /**
-     * Show the form for creating a new resource.
-     *
-     * @return \Illuminate\Http\Response
-     */
-    public function create()
-    {
-        // view "new post" page (only if logged in)
+	/**
+	 * Show the form for creating a new resource.
+	 *
+	 * @return \Illuminate\Http\Response
+	 */
+	public function create()
+	{
+		// view "new post" page (only if logged in)
 
-        $action = action('PostsController@store');
-        $method = 'POST';
+		$action = action('PostsController@store');
+		$method = 'POST';
 
-        return view('posts.create', $this->getLocalVars(get_defined_vars()));
-    }
+		return view('posts.create', $this->getLocalVars(get_defined_vars()));
+	}
 
-    /**
-     * Store a newly created resource in storage.
-     *
-     * @param  \Illuminate\Http\Request  $request
-     * @return \Illuminate\Http\Response
-     */
-    public function store(Request $request)
-    {
-        // save post to database
+	/**
+	 * Store a newly created resource in storage.
+	 *
+	 * @param  \Illuminate\Http\Request  $request
+	 * @return \Illuminate\Http\Response
+	 */
+	public function store(Request $request)
+	{
+		// save post to database
 
-        $this->validate($request, Post::$rules);
+		$this->validate($request, Post::$rules);
 
-        $post = new Post;
+		$post = new Post;
 
-        $post->created_by = mt_rand(1, 9999);
-        $post->title = $request->title;
-        $post->url = $request->url;
-        $post->content = $request->content;
+		$post->created_by = mt_rand(1, 9999);
 
-        $post->save();
+		foreach ($post->getFillable() as $column) {
+			if (isset($request[$column]) and $request[$column]) {
+				$post[$column] = $request[$column];
+			}
+		}
 
-        return redirect("posts/{$post->id}/");
-    }
+		$post->save();
 
-    /**
-     * Display the specified resource.
-     *
-     * @param  int  $id
-     * @return \Illuminate\Http\Response
-     */
-    public function show($id)
-    {
-        // view a particular post's page
+		return redirect("posts/{$post->id}/");
+	}
 
-        $post = \App\Models\Post::find($id);
+	/**
+	 * Display the specified resource.
+	 *
+	 * @param  int  $id
+	 * @return \Illuminate\Http\Response
+	 */
+	public function show($id)
+	{
+		// view a particular post's page
 
-        return view('posts.show', $this->getLocalVars(get_defined_vars()));
-    }
+		$post = Post::find($id);
 
-    /**
-     * Show the form for editing the specified resource.
-     *
-     * @param  int  $id
-     * @return \Illuminate\Http\Response
-     */
-    public function edit($id)
-    {
-        // view "edit post" page (only if logged in as admin or user who created initially)
+		return view('posts.show', $this->getLocalVars(get_defined_vars()));
+	}
 
-        $action = action('PostsController@update', $id);
-        $method = 'PUT';
-        $post = \App\Models\Post::find($id);
+	/**
+	 * Show the form for editing the specified resource.
+	 *
+	 * @param  int  $id
+	 * @return \Illuminate\Http\Response
+	 */
+	public function edit($id)
+	{
+		// view "edit post" page (only if logged in as admin or user who created initially)
 
-        return view('posts.edit', $this->getLocalVars(get_defined_vars()));
-    }
+		$action = action('PostsController@update', $id);
+		$method = 'PUT';
+		$post = Post::find($id);
 
-    /**
-     * Update the specified resource in storage.
-     *
-     * @param  \Illuminate\Http\Request  $request
-     * @param  int  $id
-     * @return \Illuminate\Http\Response
-     */
-    public function update(Request $request, $id)
-    {
-        // update post's entry in database
-        $this->validate($request, Post::$rules)
+		return view('posts.edit', $this->getLocalVars(get_defined_vars()));
+	}
 
-        return 'update post';
-    }
+	/**
+	 * Update the specified resource in storage.
+	 *
+	 * @param  \Illuminate\Http\Request  $request
+	 * @param  int  $id
+	 * @return \Illuminate\Http\Response
+	 */
+	public function update(Request $request, $id)
+	{
+		// update post's entry in database
+		$update = false;
 
-    /**
-     * Remove the specified resource from storage.
-     *
-     * @param  int  $id
-     * @return \Illuminate\Http\Response
-     */
-    public function destroy($id)
-    {
-        // delete a post (only if logged in as admin or user who created post)
-        return 'destroy post';
-    }
+		$this->validate($request, Post::$rules);
+
+		$post = Post::find($id);
+
+		foreach ($post->getFillable() as $column) {
+			if (isset($request[$column]) and $request[$column] and $request[$column] !== $post[$column]) {
+				$post[$column] = $request[$column];
+				$update = true;
+			}
+		}
+
+		if ($update) {
+			$post->save();
+		}
+
+		return redirect("posts/$id/");
+	}
+
+	/**
+	 * Remove the specified resource from storage.
+	 *
+	 * @param  int  $id
+	 * @return \Illuminate\Http\Response
+	 */
+	public function destroy($id)
+	{
+		// delete a post (only if logged in as admin or user who created post)
+		return 'destroy post';
+	}
 }
--- app/Models/Post.php
+++ app/Models/Post.php
@@ -7,29 +7,40 @@ use Carbon\Carbon;
 
 class Post extends Model
 {
-    public static $rules = [
-        'title' => 'string|max:255|required',
-        'url' => 'string|max:255|url|active_url|required_without:content',
-        'content' => 'string|required_without:url'
-    ];
+	public static $rules = [
+		'title' => 'string|min:5|max:255|required',
+		'url' => 'string|max:255|url|active_url|required_without:content',
+		'content' => 'string|min:5|required_without:url'
+	];
 
-    protected $table = 'posts';
+	protected $table = 'posts';
 
-    protected $dates = [
-        'created_at',
-        'updated_at'/*,
-        'deleted_at'*/
-    ];
+	protected $fillable = [
+		'title',
+		'url',
+		'content'
+	];
 
-    public function getTimeAgo($date)
-    {
-        return preg_replace(['~^1 day ago~', '~^1\b~'], ['yesterday', 'a'], $this[$date]->diffForHumans());
-    }
+	protected $dates = [
+		'created_at',
+		'updated_at'/*,
+		'deleted_at'*/
+	];
 
-    public function getRandomPlaceholder()
-    {
-        $placeholders = ['Guy', 'Girl', 'Fungus', 'Algae', 'Bacterium', 'Protozoa', 'Person', 'Dragon', 'Woof', 'Mew', 'Fuzz', 'Potato', 'Filename', 'Person', 'Random', '[deleted]', 'Meme', 'Insect', 'Placeholder', 'Table', 'Object', 'Floof', 'Username', 'NULL', 'Error', 'NPC', 'String', 'Function', 'Boop', 'Error: Placeholder not silly enough'];
+	public function getTimeAgo($date)
+	{
+		return preg_replace(['~^1 day ago~', '~^1\b~'], ['yesterday', 'a'], $this[$date]->diffForHumans());
+	}
 
-        return 'someRandom' . $placeholders[array_rand($placeholders)];
-    }
+	public function getRandomPlaceholder()
+	{
+		$placeholders = ['Guy', 'Girl', 'Fungus', 'Algae', 'Bacterium', 'Protozoa', 'Person', 'Dragon', 'Woof', 'Mew', 'Fuzz', 'Potato', 'Filename', 'Person', 'Random', '[deleted]', 'Meme', 'Insect', 'Placeholder', 'Table', 'Object', 'Floof', 'Username', 'NULL', 'NPC', 'String', 'Function', 'Boop', 'Error: Placeholder not silly enough'];
+
+		return 'someRandom' . $placeholders[array_rand($placeholders)];
+	}
+
+	public function getFillable()
+	{
+		return $this->fillable;
+	}
 }
